c821fc60fa48a6dc4a1c70d69ad30b4373f2916d
Adam Gruber <talknmime@gmail.com>
2014-08-25 23:00:49 -0400

    about stuff

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
   partials: {
     navbar: "partials/_navbar",
     home: "partials/_home",
-    searchNav: "partials/_searchNav"
+    searchNav: "partials/_searchNav",
+    about: "partials/_about"
   }
 };
